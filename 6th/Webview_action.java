e972db731bbc002a9bb7330c4c7656535ed15954
khg08071 <60081729+khg08071@users.noreply.github.com>
2020-05-30 01:36:14 +0900

    7th_assignment

---
+++ 6th/Webview_action.java
@@ -0,0 +1,27 @@
+package com.example.firstproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class Webview_action extends AppCompatActivity{
+
+    private WebView mWebView;
+    private WebSettings mWebSettings;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webview);
+
+        mWebView = (WebView)findViewById(R.id.webview_login);
+        mWebView.setWebViewClient(new WebViewClient());
+        mWebSettings = mWebView.getSettings();
+        mWebSettings.setJavaScriptEnabled(true);
+
+        mWebView.loadUrl("https://github.com/khg08071/khg08071.github.io");
+    }
+}
